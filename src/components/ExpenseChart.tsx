beb5ba3f8356a7a3789f9f81b54978486542c9fb
Andrés Zapata Mazo <andrez.mazo@gmail.com>
2024-11-02 21:05:38 -0500

    Se añade componende ExpenseChart para visualización de gastos por categoria, con su correspondiente css

---
+++ src/components/ExpenseChart.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { useExpenseContext } from "../context/ExpenseContext";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import "./ExpenseChart.css";
+
+// Registrar los componentes necesarios de Chart.js
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const ExpenseChart: React.FC = () => {
+  const { expenses } = useExpenseContext();
+
+  const categoryTotals = expenses.reduce((totals, expense) => {
+    totals[expense.category] = (totals[expense.category] || 0) + expense.amount;
+    return totals;
+  }, {} as { [category: string]: number });
+
+  const data = {
+    labels: Object.keys(categoryTotals),
+    datasets: [
+      {
+        label: "Gastos por Categoría",
+        data: Object.values(categoryTotals),
+        backgroundColor: "rgba(75,192,192,0.6)",
+      },
+    ],
+  };
+
+  return (
+    <div className="chart-container flex-column">
+      <h4>Resumen de Gastos</h4>
+      <Bar data={data} />
+    </div>
+  );
+};
+
+export default ExpenseChart;
