88c333e9eb9a785514ca5102bcbed290046c9ae5
Andrés Zapata Mazo <andrez.mazo@gmail.com>
2024-11-02 21:02:26 -0500

    App se añade ExpenseProvider, expenseSummary, ExpenseChart, ExpenseForm, ExpenseFilters y handlesr de filtros

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ExpenseProvider } from "./context/ExpenseContext";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseSummary from "./components/ExpenseSummary";
+import ExpenseFilters from "./components/ExpenseFilters";
+import ExpenseChart from "./components/ExpenseChart";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const handleSubmit = () => {};
+
+  // Estado para almacenar los filtros
+  const [filters, setFilters] = useState<{
+    category?: string;
+    startDate?: string;
+    endDate?: string;
+  }>({});
+
+  // Función para actualizar los filtros
+  const handleFilter = (newFilters: {
+    category?: string;
+    startDate?: string;
+    endDate?: string;
+  }) => {
+    setFilters(newFilters);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ExpenseProvider>
+      <div className="container d-flex flex-column gap-3">
+        <h1>Expense Tracker</h1>
+        <ExpenseSummary />
+        <ExpenseChart />
+        <ExpenseForm onSubmit={handleSubmit} />
+        <ExpenseFilters onFilter={handleFilter} />
+        <ExpenseList filters={filters} />{" "}
+      </div>
+    </ExpenseProvider>
   );
-}
+};
 
 export default App;
